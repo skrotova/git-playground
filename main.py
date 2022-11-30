fa8de69296d41a2a6de1a5447cc402996f6ea006
skrotova <skrotova@kse.org.ua>
2022-11-30 19:21:09 +0200

    Checking if a word was used

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ print(f"Your word is '{word}'")
 while not is_game_over():
     guess = input("Your next take: ")
 
+    if guess in guesses:
+        print("You have tried this word. Try another one")
+        continue
+
     if not guess_is_valid(guess):
         continue
 
